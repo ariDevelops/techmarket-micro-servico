7255dd21682ad32dc4cda1b59dd1d9c9b5791c42
Ariel Vieira <ariel.vieira@yandex.com>
2025-10-13 22:21:51 -0300

    inicialização do frontend

---
+++ techmarket-frontapp/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import 'bulma/css/bulma.css'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+
+function App() {
+
+  return (
+    <div className="container">
+      <h1 className="title is-1">Olá react com Bulma !</h1>
+      <button className="button is-primary">Clique aqui</button>
+    </div>
+  )
+}
+
+export default App
